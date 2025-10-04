01c2401e35e84dffd4bcab212c4762a016e6d8ae
NeelGaji <neelgajiwala79@gmail.com>
2025-10-04 15:35:51 -0400

    poll changes

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # add the polls app so Django loads templates and app configs from it
+    'polls',
 ]
 
 MIDDLEWARE = [
