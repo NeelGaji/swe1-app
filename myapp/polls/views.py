6c20da6b25475930162268e4f86bb7115619b841
NeelGaji <neelgajiwala79@gmail.com>
2025-10-04 15:35:51 -0400

    poll changes

--- myapp/polls/views.py
+++ myapp/polls/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # render a simple page with a single-select question
+    return render(request, 'polls/index.html')
