e8e88a551cd930adb1fa4cee41f368acbbd7540f
NeelGaji <neelgajiwala79@gmail.com>
2025-10-17 04:19:22 -0400

    black changes

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # add the polls app so Django loads templates and app configs from it
     "polls",
+    "polls.apps.PollsConfig",
 ]
 
 MIDDLEWARE = [
