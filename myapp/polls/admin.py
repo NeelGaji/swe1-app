8a3f9c3c6f4c38e72e5f0f1828e0da1f3c869b09
NeelGaji <neelgajiwala79@gmail.com>
2025-10-17 03:27:55 -0400

    Format code with Black

--- myapp/polls/admin.py
+++ myapp/polls/admin.py
@@ -8,9 +8,9 @@ class ChoiceInline(admin.TabularInline):
 
 
 class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('question_text', 'pub_date')
-    list_filter = ('pub_date',)
-    search_fields = ('question_text',)
+    list_display = ("question_text", "pub_date")
+    list_filter = ("pub_date",)
+    search_fields = ("question_text",)
     inlines = [ChoiceInline]
 
 
